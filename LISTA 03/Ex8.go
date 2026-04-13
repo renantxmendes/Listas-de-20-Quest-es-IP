dcc99f880e136bc4c3c9b48add72668f420c7c04
Renan Mendes <renanmendes@discente.ufg.br>
2026-04-13 19:12:02 -0300

    Create Ex8.go

---
+++ LISTA 03/Ex8.go
@@ -0,0 +1,18 @@
+package main
+
+import f "fmt"
+
+func main() {
+var soma_num int
+	
+		for i:= 1; i <=20; i++{
+			
+			f.Println("Os números dessa faixa são: ", i )
+			soma_num = soma_num + i
+		
+	}
+
+	f.Print("A soma entre os números 1 ao 20 é de: ", soma_num)
+	
+
+}
